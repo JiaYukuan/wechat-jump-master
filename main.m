06f2c1b9f6b23dbfb49732043f66b8ac229aeef6
贾宇宽 <jyk19980526@163.com>
2018-08-15 00:30:32 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
-
-    im = read_image();
-    figure; imshow(im); hold on;
-    chess = chess_middle(im);
-    board = board_middle(im, chess(1));
-    plot(chess(1), chess(2), 'x','LineWidth',2,'Color','yellow');
-    plot(board(1), board(2), 'x','LineWidth',2,'Color','red');
-    dis = distance(chess, board);
-    dis = int16(round(dis))
-    pause(1);
+    num = 0;
+    while true
+        im = read_image(num);
+        figure; imshow(im); hold on;
+        chess = chess_middle(im);
+        board = board_middle(im, chess(1));
+        plot(chess(1), chess(2), 'x','LineWidth',2,'Color','yellow');
+        plot(board(1), board(2), 'x','LineWidth',2,'Color','red');
+        dis = distance(chess, board);
+        dis = int16(round(dis))
+        pause(1);
+    end
